62dbeeef4ef7de20539c4c9d6b612c418ebeb110
Cristian Ratoi <cristian.ratoi@vitals.co>
2023-05-23 11:29:46 +0300

    refactor: update API URL

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-export const messageApiUrl = 'https://interview-message-api.herokuapp.com'; // { message: string }
+export const messageApiUrl = 'https://interview-problems.onrender.com'; // { message: string }
 
 // write code to display message based on selected option
